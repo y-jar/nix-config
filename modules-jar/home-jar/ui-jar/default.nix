0b3a879e2dec2a465e0353ce0a18530e29f52daf
y-jar <park.7qs@gmail.com>
2026-05-02 15:50:27 -0400

    [fixed stray desktop var]

--- modules-jar/home-jar/ui-jar/default.nix
+++ modules-jar/home-jar/ui-jar/default.nix
@@ -2,7 +2,8 @@
 
 {
     # This imports the modules depending on what the user picked within the main flake.nix.
-    imports = [ ]
-    ++ (if desktop == "sway" then [ ./sway-bin/default.nix ] else [ ])
-    ++ (if desktop == "niri" then [ ./niri-bin/default.nix ] else [ ]);
+    imports = [
+        ./niri-bin/default.nix
+        ./sway-bin/default.nix
+     ];
 }
