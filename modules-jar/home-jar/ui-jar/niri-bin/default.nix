569dbb1200767dc43f4ce0a034a6dd4767d7f0c8
y-jar <park.7qs@gmail.com>
2026-05-05 15:26:37 -0400

    [flipped-shark update]

--- modules-jar/home-jar/ui-jar/niri-bin/default.nix
+++ modules-jar/home-jar/ui-jar/niri-bin/default.nix
@@ -3,9 +3,12 @@
 {
   # import
   imports = [ ]
-    ++ (if hostnm == "aari" then [ ./niri-aanri.nix ] else [ ])
-    ++ (if hostnm == "calender" then [ ./niri-calender.nix ] else [ ])
-    ++ (if hostnm == "flipped-shark" then [ ./niri-cold-flip.nix ] else [ ])
-    ++ (if hostnm == "tyun" then [ ./niri-tyun.nix ] else [ ])
-    ++ (if hostnm == "yilyonix" then [ ./niri-yilyonix.nix ] else [ ./niri-base.nix ./waybar.nix ]);
+    ++ (if hostnm == "aanri" then [ ./niri-aanri.nix ] else [])
+    ++ (if hostnm == "calender" then [ ./niri-calender.nix ] else [])
+    ++ (if hostnm == "flipped-shark" then [ ./niri-flipped-shark.nix ] else [])
+    ++ (if hostnm == "tyun" then [ ./niri-tyun.nix ] else [])
+    ++ (if hostnm == "yilyonix" then [ ./niri-yilyonix.nix ] else [])
+    # base is only loaded if no host-specific config matched above
+    ++ (if builtins.elem hostnm [ "aanri" "calender" "flipped-shark" "tyun" "yilyonix" ]
+        then [] else [ ./niri-base.nix ]);
 }
