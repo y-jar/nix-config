03a76208d6c5f667376b9577c4fc0c5e4780c3c8
y-jar <park.7qs@gmail.com>
2026-06-09 19:45:29 -0400

    uhg

--- modules/host-jar/yilyonix/default.nix
+++ modules/host-jar/yilyonix/default.nix
@@ -1,6 +1,10 @@
-{ inputs, hostnm, ... }:
 {
-  inputs = [
+  inputs,
+  hostnm,
+  ...
+}:
+{
+  imports = [
     ./hardware-configuration.nix
     inputs.home-manager.nixosModules.home-manager # Pulls in home-manager module
   ];
@@ -24,8 +28,12 @@
   networking.hostName = "${hostnm}"; # sets hostname
 
   # boot
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    }; # end of loader
+  }; # end of boot
 
   # the aliases per system
   environment.shellAliases = {
