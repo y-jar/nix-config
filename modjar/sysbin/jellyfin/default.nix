26fd88d88648630955eff72c99b4e2de75cbb5e3
jar <jar>
2026-06-16 13:05:21 -0400

    [added jellyfin config, more will be added maybe?] [more documentation will come as i actually try out jellyfin on an up coming host `whale`]

---
+++ modjar/sysbin/jellyfin/default.nix
@@ -0,0 +1,57 @@
+# ref: https://wiki.nixos.org/wiki/Jellyfin
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.sysSettings.server.jellyfin;
+in
+{
+  options = {
+    sysSettings.server.jellyfin = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Enable Jellyfin server";
+      }; # end of jellyfin.enable
+      juser = lib.mkOption {
+        type = lib.types.str;
+        default = "";
+        description = "Jellyfin user";
+      }; # end of jellyfin.juser
+    }; # end of jellyfin
+  }; # end of options
+
+  config = lib.mkIf cfg.enable {
+    services.jellyfin = {
+      enable = true;
+      openFirewall = true;
+      user = cfg.juser;
+
+      # hardware acceleration
+      # hardwareAcceleration = {
+      #   enable = true;
+      #   type = "nvenc";
+      #   device = "/dev/dri/by-path/pci-0000:01:00.0-render"; # device path for hardware acceleration
+      # }; # end of hardwareAcceleration
+    }; # end of services.jellyfin
+
+    # packages
+    environment.systemPackages = [
+      pkgs.jellyfin
+      pkgs.jellyfin-desktop
+      pkgs.jellyfin-web
+      pkgs.jellyfin-ffmpeg
+    ]; # end of environment.systemPackages
+
+    # user
+    # sets the jellyfin group to include all users
+    users.groups = {
+      jellyfin = {
+        members = config.sysSettings.users;
+      }; # end of jellyfin
+    }; # end of users.groups
+  }; # end of config
+}
