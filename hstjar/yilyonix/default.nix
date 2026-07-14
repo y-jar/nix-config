b893fb10755a05977c60e6cc1cbdba79fdcb3ad5
y-jar <park.7qs@gmail.com>
2026-07-14 13:13:17 -0400

    remove home-manager for each host

--- hstjar/yilyonix/default.nix
+++ hstjar/yilyonix/default.nix
@@ -7,41 +7,8 @@
 
 {
   imports = [
-    inputs.home-manager.nixosModules.home-manager # home-manager module
     ./system.nix # system configuration
     ./hardware-configuration.nix # hardware configuration
-    ./boot.nix
+    ./boot.nix # boot settings
   ];
-
-  # config for home-manager and user entry and user system settings
-  config = {
-    # pass important args
-    home-manager = {
-      useGlobalPkgs = true; # use global pkgs instead of user pkgs
-      useUserPackages = true; # use user pkgs instead of global pkgs
-      backupFileExtension = "backup"; # file extension for backups
-
-      # args passed to home-manager
-      extraSpecialArgs = {
-        inherit inputs;
-        inherit hostnm;
-        gnomeEnable = config.sysSettings.gnome.enable;
-        hyprlandEnable = config.sysSettings.hyprland.enable;
-        niriEnable = config.sysSettings.niri.enable;
-        aiEnable = config.sysSettings.ai.enable;
-      }; # end of home-manager extraSpecialArgs
-    }; # end of home-manager base config
-
-    home-manager.users = builtins.listToAttrs (
-      map (user: {
-        name = user;
-        value = {
-          imports = [
-            ./user.nix # loads user information settings
-            ../../modjar/usrbin # knowing user settings, loads user system configuration
-          ];
-        };
-      }) config.sysSettings.users
-    ); # end of home-manager users
-  }; # end of config
 }
