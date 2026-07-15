493f633e2a3da03ae7eb0c9354e38cfcc5911164
y-jar <park.7qs@gmail.com>
2026-07-15 09:54:48 -0400

    add a new dir for modules that grab and save my stuff on PCs

---
+++ modjar/usrbin/resYoink/default.nix
@@ -0,0 +1,42 @@
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+let
+  cfg = config.usrSettings.resYoink;
+in
+{
+  options.usrSettings.resYoink = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable resource symlinks (wallpapers, icons, pfps)";
+    };
+    wallpapers = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Symlink wall-jar into picjar/";
+    };
+    icons = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Symlink icon-jar into picjar/";
+    };
+    profilePictures = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Symlink pfp-jar into picjar/";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.file = lib.mkMerge [
+      (lib.mkIf cfg.wallpapers { "picjar/wall-jar".source = inputs.wall-jar; })
+      (lib.mkIf cfg.icons { "picjar/icon-jar".source = inputs.icon-jar; })
+      (lib.mkIf cfg.profilePictures { "picjar/pfp-jar".source = inputs.pfp-jar; })
+    ];
+  };
+}
