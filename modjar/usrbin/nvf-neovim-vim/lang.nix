6c3034bfd537265b1d7756e36021b616f1fd173a
y-jar <park.7qs@gmail.com>
2026-07-14 23:09:20 -0400

    Update lang.nix

--- modjar/usrbin/nvf-neovim-vim/lang.nix
+++ modjar/usrbin/nvf-neovim-vim/lang.nix
@@ -5,95 +5,41 @@
 }:
 {
   config = lib.mkIf config.usrSettings.editors.nvf.enable {
-    programs.nvf = {
-      settings = {
-        vim = {
-          languages = {
-            bash = {
-              enable = true;
-              lsp.servers.bashls = { };
-              treesitter.enable = true;
-            };
-            csharp = {
-              enable = true;
-              treesitter.enable = true;
-            };
-            css = {
-              enable = true;
-              lsp.servers.cssls = { };
-              treesitter.enable = true;
-            };
-            go = {
-              enable = true;
-              lsp.servers.gopls = {
-                settings = {
-                  analyses = { unusedparams = true; };
-                  staticcheck = true;
-                };
-              };
-              treesitter.enable = true;
-            };
-            python = {
-              enable = true;
-              lsp.servers.pyright = {
-                settings = {
-                  python.analysis.typeCheckingMode = "basic";
-                };
-              };
-              treesitter.enable = true;
-            };
-            rust = {
-              enable = true;
-              lsp.servers.rust-analyzer = {
-                settings = {
-                  cargo.buildScripts.enable = true;
-                  procMacro.enable = true;
-                };
-              };
-              treesitter.enable = true;
-            };
-            html = {
-              enable = true;
-              treesitter.enable = true;
-            };
-            json = {
-              enable = true;
-              lsp.servers.jsonls = { };
-              treesitter.enable = true;
-            };
-            lua = {
-              enable = true;
-              lsp.servers.lua-ls = {
-                settings = {
-                  Lua = {
-                    diagnostics.globals = [ "vim" ];
-                    workspace.checkThirdParty = false;
-                  };
-                };
-              };
-              treesitter.enable = true;
-            };
-            markdown = {
-              enable = true;
-              treesitter.enable = true;
-            };
-            nix = {
-              enable = true;
-              lsp.servers.nil = {
-                settings = {
-                  formatting.command = [ "nixfmt" ];
-                };
-              };
-              treesitter.enable = true;
-            };
-            yaml = {
-              enable = true;
-              lsp.servers.yamlls = { };
-              treesitter.enable = true;
-            };
-          }; # end of languages
-        }; # end of vim
-      }; # end of settings
-    }; # end of nvf
+    programs.nvf.settings.vim = {
+      languages = {
+        bash.enable = true;
+        csharp.enable = true;
+        css.enable = true;
+        go.enable = true;
+        python.enable = true;
+        rust.enable = true;
+        html.enable = true;
+        json.enable = true;
+        lua.enable = true;
+        markdown.enable = true;
+        nix.enable = true;
+        yaml.enable = true;
+      }; # end of languages
+
+      # Minimal LSP server overrides
+      lsp.servers = {
+        nil.settings = {
+          formatting.command = [ "nixfmt" ];
+        };
+        pyright.settings = {
+          python.analysis.typeCheckingMode = "basic";
+        };
+        lua-ls.settings = {
+          Lua.diagnostics.globals = [ "vim" ];
+        };
+        rust-analyzer.settings = {
+          cargo.buildScripts.enable = true;
+        };
+        gopls.settings = {
+          analyses = { unusedparams = true; };
+          staticcheck = true;
+        };
+      }; # end of lsp.servers
+    }; # end of vim
   }; # end of config
 }
