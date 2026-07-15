0b2c49af1f542f1b9cf5ccbfce35d70f7c82b73b
y-jar <park.7qs@gmail.com>
2026-07-15 09:52:46 -0400

    Update default.nix

--- modjar/usrbin/editors/default.nix
+++ modjar/usrbin/editors/default.nix
@@ -20,7 +20,7 @@ in
 
   imports = [
     ./helix.nix # helix config from: https://github.com/ryan4yin/nix-config
-    ./nvf-neovim.nix # my neovim config via nvf
+    ./nvf-neovim-vim # my neovim config via nvf
   ]; # end of imports
 
   config = lib.mkIf cfg.enable {
