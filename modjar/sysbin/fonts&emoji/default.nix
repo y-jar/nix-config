15092f38a0d34a233897d062a708ce5cc3a68dc4
y-jar <park.7qs@gmail.com>
2026-07-27 11:39:09 -0400

    twk, startups, added font

--- modjar/sysbin/fonts&emoji/default.nix
+++ modjar/sysbin/fonts&emoji/default.nix
@@ -8,13 +8,16 @@
       comfortaa # Clean and modern font suitable for headings and logos
       cascadia-code # Monospaced font that includes programming ligatures and is designed to enhance the modern look and feel of the Windows Terminal
       excalifont # Font based on the original handwritten Virgil font carefully curated to improve legibility while preserving its hand-drawn nature
-      noto-fonts-emoji-blob-bin # Emoji font based on the Noto Emoji blob [ref:whisper]
+      monocraft #Programming font based on the typeface used in Minecraft
 
       # [jp]
       ipaexfont # japanese font
       rounded-mgenplus # Japanese font based on Rounded M+ and Noto Sans Japanese
       koruri # Japanese TrueType font obtained by mixing M+ FONTS and Open Sans
       #nerd-fonts.m+ # Nerd Fonts: Multiple styles and weights, many glyph sets (e.g. Kana glyphs)
+
+      # [emojis]
+      noto-fonts-emoji-blob-bin # Emoji font based on the Noto Emoji blob [ref:whisper]
     ]; # end of packages
 
     # emoji
